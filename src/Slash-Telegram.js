24b35eacb0df8093f1f518d48510234a901d586d
Rodolfo Echenique <echeniquer@gmail.com>
2023-02-23 14:44:26 -0300

    cambios generados

--- src/Slash-Telegram.js
+++ src/Slash-Telegram.js
@@ -1,7 +1,7 @@
 const TelegramBotToken = "5700511360:AAG0li9KsWNfznOQEkZx-a4LX5KaPsuqEww";
 const TelegramChatID = "-1001532010755";
 
-const sendTelegramMessage = (user="Rodolfo Echenique", text="Mensaje de Prueba") => {
+const sendTelegramMessage = (user, text) => {
   const message = `${user} esta alertando: ${text}`;
   const response = UrlFetchApp.fetch(`https://api.telegram.org/bot${TelegramBotToken}/sendMessage\?chat_id=${TelegramChatID}&parse_mode=HTML&text=${message}`);
   console.log(response);
